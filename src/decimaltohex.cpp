f154509f7bb7e90f2c907579df35ec008a6cb167
PNguyen <Ngocphunguyenabc@gmail.com>
2024-03-21 07:37:52 +0000

    feat: :sparkles: Decimal To Hex Conversion
    This feature is made by Chi Thinh

--- src/decimaltohex.cpp
+++ src/decimaltohex.cpp
@@ -2,6 +2,8 @@
 #include <math.h>
 #include <regex>
 #include <string>
+#include <stack>
+#include <queue>
 std::string DecimalToHex::ConvertIntegerPartToHex(std::string integerPart)
 {
     if (integerPart == "0")
@@ -11,8 +13,19 @@ std::string DecimalToHex::ConvertIntegerPartToHex(std::string integerPart)
     else
     {
         /*Code here*/
-        /*Chi Thinh*/
-        return "";
+        /*Chi Thinh done*/
+        auto defaultintegerPart = stoll(integerPart);
+        std::stack<long long> st;
+        while (defaultintegerPart != 0) {
+            st.push(defaultintegerPart % _hexBase);
+            defaultintegerPart /= _hexBase;
+        }
+        std::string result;
+        while(!st.empty()){
+            result += ConvertToHex(st.top());
+            st.pop();
+        }
+        return result;
     }
 }
 
@@ -25,24 +38,50 @@ std::string DecimalToHex::ConvertFractionalPartToHex(std::string fractionalPart,
     else
     {
         /*Code here*/
-        /*Chi Thinh*/
-        return "";
+        /*Chi Thinh done*/
+        long long length = fractionalPart.length();
+        auto hexadecimal = std::stold(std::string("0." + fractionalPart));
+        int defaultPrecision = 0;
+
+        if (precision != -1)
+        {
+            defaultPrecision = precision;
+        }
+        else
+        {
+            defaultPrecision = _defaultPrecision;
+        }
+        std::queue<int> q;
+        while(hexadecimal > 0 && defaultPrecision-- > 0)
+        {
+            hexadecimal *= _hexBase;
+            q.push(hexadecimal);
+            hexadecimal -= q.back();
+        }
+        std::string result;
+        while(!q.empty())
+        {
+            result += ConvertToHex(q.front());
+            q.pop();
+        }
+        return result;
     }
+
 }
 
 /*helpfull function*/
-// char DecimalToHex::ConvertToHex(int number)
-// {
-//     if (number >= 0 && number <= 9)
-//     {
-//         return static_cast<char>(number + '0');
-//     }
-//     else
-//     {
-//         return static_cast<char>(number - 10 + 'A');
-//     }
-//     throw std::invalid_argument("Number must be between 0 and 15");
-// }
+char DecimalToHex::ConvertToHex(int number)
+{
+    if (number >= 0 && number <= 9)
+    {
+        return static_cast<char>(number + '0');
+    }
+    else
+    {
+        return static_cast<char>(number - 10 + 'A');
+    }
+    throw std::invalid_argument("Number must be between 0 and 15");
+}
 
 DecimalToHex::DecimalToHex()
 {
